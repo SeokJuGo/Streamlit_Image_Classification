5b59dfae6f1519f53c7bbb1d0e03753348f09c1d
SeokJuGo <116260619+SeokJuGo@users.noreply.github.com>
2023-01-27 11:10:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from PIL import Image
 
 st.set_option('deprecation.showfileUploaderEncoding', False) # deprecation 표시 안함 
 st.title("머신러닝 이용 뇌종양 MRI 사진 판독 서비스")
-st.markdown("""
+st.markdown(""" 
 뇌종양 MRI 사진을 분류합니다. 
 이 서비스는 의사들의 뇌 MRI 사진 판독의 도우미일 뿐입니다. 
 정확한 최종 진단 결과는 반드시 전문 담당 의사의 확인과 승인을 거치십시요.""")
